cd6ab7826f96c3f9a3dc8ea9b4ce3be37e59768e
Paul Hammant <paul@hammant.org>
2017-04-19 07:41:24 -0400

    kill blank lines in not-really json

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,7 @@
 var app= angular.module("MyApp", []);
 app.controller('MyController', function($scope, $http) {
     $http.get(window.location.hash.slice(1) + "-gh-pages.txt").then(function(response) {
-        $scope.repos = JSON.parse("{ \"res\": [" + response.data.slice(0, -1) + "]}").res
+        $scope.repos = JSON.parse("{ \"res\": [" + response.data.trim().slice(0, -1) + "]}").res
     });
 });
 
